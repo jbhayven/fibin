ae767bfb4706b59cd818e67b39183ebe9f02a66b
Przemysław <pp406298@students.mimuw.edu.pl>
2019-12-01 23:32:10 +0100

    kod po pierwszej próbie zaklepania całości; zbyt dużo założeń, zbyt mało ogólności, nie działa tak, jak powinien dobry język funkcyjny

--- fibin.cpp
+++ fibin.cpp
@@ -1,40 +1,134 @@
-template<unsigned int N> struct Fibo {
-    enum { val = Fibo<N-1>::val + Fibo<N-2>::val };
+#include <cstdint>
+
+template<unsigned int N, typename T> 
+struct Fib_eval {
+    static constexpr T val = Fib_eval<N-1, T>::val + Fib_eval<N-2, T>::val;
 };
-template<> struct Fibo<0> {
-    enum { val = 0 };
+template<typename T> 
+struct Fib_eval<0, T > {
+    static constexpr T val = 0;
 };
-template<> struct Fibo<1> {
-    enum { val = 1 };
+template<typename T> 
+struct Fib_eval<1, T > {
+    static constexpr T val = 1;
 };
+
 ////////////////////
 
 struct True {};
 struct False {};
 
-template<typename T> struct Lit { };
+template<unsigned int N> struct Fibo {};
 
-template<> struct Lit<True> { 
-    enum { val = 1 };
+template<typename T, typename... context > 
+struct Lit { };
+
+template<typename... context> 
+struct Lit<True, context...> { 
+    static constexpr bool val = true;
 };
 
-template<> struct Lit<False> {
-    enum { val = 0 };
+template<typename... context> 
+struct Lit<False, context...> { 
+    static constexpr bool val = false;
 };
 
-template<unsigned int U> struct Lit< Fibo<U> > {
-    enum { val = Fibo<U>::val };
+template<unsigned int U, typename... context, typename T> 
+struct Lit< Fibo<U>, T, context... > {
+    static constexpr auto val = Fib_eval<U, T>::val;
 };
 
 ////////////////////
-template<typename Condition, typename IfTrue, typename IfFalse> 
+
+template<typename T, typename... context>
+struct evaluate {};
+
+template< typename... Targs, template<typename...> typename T, typename... context >
+struct evaluate<T<Targs...>, context...> {
+    static constexpr auto val = T<Targs..., context...>::val;
+};
+
+template< int i, typename... Targs, template<int, typename...> typename T, typename... context >
+struct evaluate<T<i, Targs...>, context...> {
+    static constexpr auto val = T<i, Targs..., context...>::val;
+};
+
+////////////////////
+
+template<typename Condition, typename IfTrue, typename IfFalse, typename... context> 
 struct If {
-    enum { val = Condition::val ? IfTrue::val : IfFalse::val }; 
+    static constexpr auto val = 
+        evaluate <Condition, context...>::val ?
+        evaluate <IfTrue, context...>::val :
+        evaluate <IfFalse, context...>::val;
 };
 
 ////////////////////
 
-constexpr int Var(const char *name) {
+template<typename Left, typename Right, typename... context>
+struct Eq {
+    static constexpr bool val = 
+        (evaluate<Left, context...>::val == evaluate<Right, context...>::val);
+};
+
+////////////////////
+
+using VarID = uint32_t;
+
+template<VarID id, typename Value> 
+struct Variable { };
+
+////////////////////
+
+template <VarID Var, typename Value, typename Expression, typename... context> 
+struct Let { 
+    static constexpr bool val =
+        evaluate<Expression, Variable<Var, Value>, context...>::val; 
+};
+
+////////////////////
+
+#include <type_traits>
+
+template<VarID Var, typename... context>
+struct Lookup_evaluate { };
+
+template<VarID Var, VarID id, typename Value, template<VarID, typename> typename Variable, typename... context> 
+struct Lookup_evaluate<Var, Variable<id, Value>, context...> {
+    static constexpr auto val = 
+        Var == id ?
+        evaluate<Value, context...>::val :
+        Lookup_evaluate<Var, context...>::val;
+};
+
+////////////////////
+
+template <VarID Var, typename... context>
+struct Ref {
+    static constexpr auto val = Lookup_evaluate<Var, context...>::val;
+};
+
+////////////////////
+
+template <VarID Var, typename Body, typename... context>
+struct Lambda { };
+
+////////////////////
+
+template <typename Fun, typename Param, typename... context>
+struct Invoke { };
+
+template < VarID id, typename Body, template <VarID, typename> typename Lambda, typename Param, typename... context>
+struct Invoke<Lambda<id, Body>, Param, context...> { 
+    static constexpr auto val = evaluate<Body, Variable<id, Param>, context...>::val;
+};
+
+////////////////////
+
+template<typename... args> struct Sum{};
+
+
+constexpr VarID Var(const char *name) {
     int returned = 0;
     
     int i = 0; 
@@ -72,34 +166,25 @@ constexpr int Var(const char *name) {
     return returned;
 }
 
-template<typename... args> struct Sum{};
-
-template<int i, typename T, typename U> struct Let {};
-
-template<int i, typename T, typename... args>
-struct Let<i, T, Sum<args...> > {
-    enum { val = Sum<args..., Fibo<i>, T>::val }; 
-};
-
-template<typename T> struct Invoke { };
-
+#include <typeinfo>
+#include <iostream>
 
 template< typename Number >
 class Fibin {
     public:
     
-    template< typename T >
-    static constexpr Number eval() {
-        return T::val;
+    static void eval() {
+        std::cout << "Fibin doesn't support this type!\n";
     }
     
+    template< typename T, typename = typename std::enable_if_t<std::is_integral<Number>::value > >
+    static constexpr Number eval() {
+        return evaluate<T, Number>::val;
+    }
 };
 #include <stdio.h>
 
 int main() {
-    printf("%d\n", Fibin<unsigned int>::eval< Fibo<Var("a")> >() );
-    printf("%d\n", Fibin<unsigned int>::eval< Fibo<3> >() );
-    printf("%d\n", Fibin<unsigned int>::eval< Fibo<5> >() );
     
     printf("%d\n", Fibin<unsigned int>::eval< Lit<Fibo<6> > > () );
     
@@ -110,10 +195,38 @@ int main() {
             Lit<False>,
             Lit<True>
         >, 
-        Fibo<10>, 
-        Fibo<2> 
+        Lit<Fibo<10>>, 
+        Lit<Fibo<2>> 
     > 
     >() );
+
+    /*
+    printf("%llu\n", Fibin<uint64_t>::eval<
+        Invoke<
+            Let<
+                Var("x"), 
+                Lit< Fibo<0> >, 
+                Lambda<
+                    Var("x"), 
+                    Ref<Var("x")>
+                >
+            >, 
+            Lit< Fibo<1> >
+            >
+        >());
+        */
+    /*
+    printf("%llu\n", Fibin<uint64_t>::eval<
+        Let<
+            Var("f"),
+            Lambda<
+                Var("x"),
+                Ref<Var("x")>
+            >,
+            Invoke<Ref<Var("f")>, Lit<Fibo<0> > >
+        >
+    >());
+    */
     
     //printf("%d\n", Fibin<unsigned int>::eval< Lit<int> > () );
     
