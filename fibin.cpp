256d3a716a9b90197e997fdc16f87d07ec304f3d
Przemysław <pp406298@students.mimuw.edu.pl>
2019-12-01 16:22:29 +0100

    próbujemy ze zmiennymi

--- fibin.cpp
+++ fibin.cpp
@@ -8,10 +8,11 @@ template<> struct Fibo<1> {
     enum { val = 1 };
 };
 
+////////////////////
 class True {};
 class False {};
 
-template<typename T> struct Lit { Lit<T>() = delete; };
+template<typename T> struct Lit { };
 
 template<> struct Lit<True> { 
     enum { logical = 1 };
@@ -24,7 +25,58 @@ template<> struct Lit<False> {
 template<unsigned int U> struct Lit< Fibo<U> > {
     enum { val = Fibo<U>::val };
 };
+////////////////////
+
+constexpr int Var(const char *name) {
+    int returned = 0;
     
+    int i = 0; 
+    while(i < 6 && name[i] != 0) {
+        if(!(
+            ('0' <= name[i] && name[i] <= '9') || 
+            ('a' <= name[i] && name[i] <= 'z') ||
+            ('A' <= name[i] && name[i] <= 'Z')
+            )
+        )
+        returned = -1;
+        i++;
+    }
+
+    if(i < 1 || i > 6) returned = -1;
+    
+    if(returned == 0){
+        int result = 0;
+        int factor = 1;
+        
+        while(i--) {
+            if('0' <= name[i] && name[i] <= '9') 
+                result += (name[i] - '0') * factor;
+            else if('A' <= name[i] && name[i] <= 'Z')
+                result += (10 + name[i] - 'A') * factor;
+            else
+                result += (10 + name[i] - 'a') * factor;          
+            
+            factor *= (('z' - 'a' + 1) + ('9' - '0' + 1));
+        }
+
+        returned = result;
+    }
+    
+    return returned;
+}
+
+template<typename... args> struct Sum{};
+
+template<int i, typename T, typename U> struct Let {};
+
+template<int i, typename T, typename... args>
+struct Let<i, T, Sum<args...> > {
+    enum { val = Sum<args..., Fibo<i>, T>::val }; 
+};
+
+template<typename T> struct Invoke { };
+
+
 template< typename Number >
 class Fibin {
     public:
@@ -34,11 +86,10 @@ class Fibin {
         return T::val;
     }
 };
-
 #include <stdio.h>
 
 int main() {
-    printf("%d\n", Fibin<unsigned int>::eval< Fibo<0> >() );
+    printf("%d\n", Fibin<unsigned int>::eval< Fibo<Var("a")> >() );
     printf("%d\n", Fibin<unsigned int>::eval< Fibo<3> >() );
     printf("%d\n", Fibin<unsigned int>::eval< Fibo<5> >() );
     
